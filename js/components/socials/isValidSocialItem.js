47cd2adbd47ac071197f269224043b1aba6473df
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-11-26 23:13:28 +0200

    js update

--- js/components/socials/isValidSocialItem.js
+++ js/components/socials/isValidSocialItem.js
@@ -1,26 +1,24 @@
 /**
- * RenderSocial() funkcijos metu vykdomame cikle gaunamu duomenu validacija
- * @param {object} itemObject objektas aprasantis viena social nuoroda, kuri sudaro icona ir nuoroda (href)
+ * renderSocial() funkcijos metu vykdomame cikle gaunamu duomenu validacija
+ * @param {object} itemObject Objektas aprasantis viena social nuoroda, kuri sudaro ikona ir nuoroda (href)
  * @returns {boolean} Jei patikrinimo metu randama logine duomenu klaida, tai grazina `false`, priesingu atveju - `true`
  */
-
 function isValidSocialItem(itemObject) {
-    if(typeof itemObject !== 'object') {
-        console.warn ('ERROR: social elemento turetu buti objektas');
+    if (typeof itemObject !== 'object') {
+        console.warn('ERROR: social elemento turetu buti objektas');
+        return false;
+    }
+    if (typeof itemObject.link !== 'string' ||
+        itemObject.link === '') {
+        console.warn('ERROR: social elemento nuoroda turetu buti tekstine ir ne tuscia');
         return false;
     }
     if (typeof itemObject.icon !== 'string' ||
-     itemObject.icon === '') {
-        console.warn ('ERROR: social ikona  turetu buti tekstine ir ne tuscia');
+        itemObject.icon === '') {
+        console.warn('ERROR: social elemento ikona turetu buti tekstine ir ne tuscia');
         return false;
     }
-    if(typeof itemObject.link !== 'string' ||
-     itemObject.link === '') {
-        console.warn ('ERROR: social elemento turetu buti tekstine ir ne tuscia');
-        return false;
-    }
-
-    return true; 
+    return true;
 }
 
-export {isValidSocialItem}
+export { isValidSocialItem }
--- js/components/socials/renderSocials.js
+++ js/components/socials/renderSocials.js
@@ -1,53 +1,44 @@
-import { isinputValid } from "../isinputValid.js"; /* susiimportafom if validacija, kuri yra kitame faile ir kuri validuos gautus duomenis dar pries paluriant funkcija */
-import {isValidSocialItem} from "./isValidSocialItem.js";
+import { isInputValid } from "./isInputValid.js";
+import { isValidSocialItem } from './isValidSocialItem.js';
 
 /**
  * Social nuorodu generavimas is pateiktu duomenu i nurodyta vieta DOM'e.
- * @param {string} selector selektorus, kaip rasti norima vieta, kaip bus istatomas sugeneruotas kodas
- * @param {Array} data duomenu  masyvas su objektais, kurie reprezentuoja social nuorodas
- * @returns {boolean} Logikos vykdymo metu radus klaida grazinamas `false`, o funkcijai suveikus teisingai - `true`
+ * @param {string} selector Selectorius, kaip rasti norima vieta, kur bus istatomas sugeneruotas kodas
+ * @param {Array} data Duomenu masyvas su objektais, kurie reprezentuoja social nuorodas
+ * @returns {boolean} Logikos vykdymo metu radus klaida grazinas `false`, o funkcijai suveikus teisingai - `true`
  */
-function renderSocials(selector, data) {  /* pradziai pasirenka selector i kur eis duomenys ir poto jau duomenis(data) */
-    if (!isinputValid(selector, data)) {
+function renderSocials(selector, data) {
+    // input validation
+    if (!isInputValid(selector, data)) {
         return false;
     }
-    
 
-//  logic
-    const socialsDOM = document.querySelector(selector); /* selector reiskia ta pati kaip querySelector('footer > .row) */
-
-    // jeigu nera priekyje socialsDOM, tumet ismetam error 'nerasta turinio generavimo vieta' ir jeigu jis randa klaida, jis toliau net neleidzia uzsisukti let funkcijai, taupydamas resursus.
-    if(!socialsDOM) {
+    // logic
+    const socialsDOM = document.querySelector(selector);
+    if (!socialsDOM) {
         console.error('ERROR: nerasta turinio generavimo vieta');
         return false;
     }
+
     let HTML = '';
 
     for (let i = 0; i < data.length; i++) {
         const item = data[i];
-        // jeigu sita funkcija if iskvietus ir jam davus 'item' i taji nari ir jeigu jis yra ne validus, tuomet sakau 'continue' tesk darba ir eik prie kito saraso nario. Priesingu atveju ta bloga item prisumuos prie HTML kito nario.
-        if(!isValidSocialItem(item)) {
+        if (!isValidSocialItem(item)) {
             continue;
-
         }
         HTML += `<a href="${item.link}" target="_blank" class="fa fa-${item.icon}" aria-hidden="true"></a>`;
     }
+
     // post logic validation
-   if( HTML === '') {
-       console.error ('ERROR: nepavyko sugeneruoti social ikonu/nuorodu.')
-       return false;
-   }
-    
+    if (HTML === '') {
+        console.error('ERROR: nepavyko sugeneruoti social ikonu/nuorodu.');
+        return false
+    }
 
-
-
-
-
-// return
-
+    // return
     socialsDOM.innerHTML = HTML;
     return true;
-
 }
 
 export { renderSocials }
