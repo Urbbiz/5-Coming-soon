9a369710f48d7f057914580af8fdce58bda57bd3
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-11-27 10:01:32 +0200

    socials icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { renderSocials } from "./components/socials/renderSocials.js";
 import { socialsData } from './data/socialsData.js';
 
-renderSocials('footer > .row',socialsData); /* footer > .row', nurodo vieta kur naudojamas bus failas ir jis pasiema logika. gali buti ir kitas failas, nurodantis i kita vieta, ir naudotis ta pacia logika, tik su kitais duomenimis) */
+renderSocials('footer > .row', socialsData);
+
+/* footer > .row', nurodo vieta kur naudojamas bus failas ir jis pasiema logika. gali buti ir kitas failas, nurodantis i kita vieta, ir naudotis ta pacia logika, tik su kitais duomenimis) */
 
